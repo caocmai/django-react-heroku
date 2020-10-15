a65f899c50e80f56d6a65777429fc7d148a9527d
Cao Mai <caomai@rocketmail.com>
2020-10-15 15:19:36 -0700

    [add] bootstrap buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import test from "./test.png";
+import { Button } from 'react-bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
@@ -8,6 +10,14 @@ const App = () => {
       <div>Hello, World!</div>
       <img src={logo} alt="Logo" />
       <img src={test} alt="TEST image" />
+      <Button bsStyle="primary">Primary</Button>{' '}
+      <Button type="button" class="btn btn-primary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+
+      <button type="button" class="btn btn-primary">Primary BOotStrap</button>
+<button type="button" class="btn btn-secondary">Secondary BOotstap</button>
+
     </div>
 
   )
